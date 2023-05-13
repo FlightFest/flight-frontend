7539b786e76dc2f5118897bf259ca6690e7f90be
Arnau <arnaumoran@gmail.com>
2023-05-13 23:02:23 +0200

    updates

--- app/src/main/java/NewPlayerActivity.java
+++ app/src/main/java/NewPlayerActivity.java
@@ -1,42 +1,26 @@
-import android.Manifest;
 import android.app.Activity;
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothSocket;
-import android.content.pm.PackageManager;
 import android.widget.EditText;
-
-import androidx.core.app.ActivityCompat;
 
 import com.example.vuelin_go_front.R;
 
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.UUID;
+import java.net.Socket;
 
 public class NewPlayerActivity extends Activity {
 
-    private static final UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"); // replace with your UUID
+    private static final String SERVER_IP = "192.168.0.100"; // replace with your server IP address
+    private static final int SERVER_PORT = 8080; // replace with your server port number
 
-    private static final int REQUEST_BLUETOOTH_CONNECT_PERMISSION = 1;
-
-    private BluetoothSocket socket;
+    private Socket socket;
     private OutputStream outputStream;
-    private BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
     private EditText input;
 
-    public void connectToServer(String address) {
-        BluetoothDevice device = bluetoothAdapter.getRemoteDevice(address);
+    public void connectToServer() {
         try {
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.BLUETOOTH_CONNECT}, REQUEST_BLUETOOTH_CONNECT_PERMISSION);
-            } else {
-                // Permission has already been granted, proceed with Bluetooth connection
-                connectToServer(address);
-            }
-            socket = device.createRfcommSocketToServiceRecord(uuid);
-            socket.connect();
+            // Create socket connection to server
+            socket = new Socket(SERVER_IP, SERVER_PORT);
             outputStream = socket.getOutputStream();
 
             input = (EditText) findViewById(R.id.player_name_input);
@@ -55,4 +39,5 @@ public class NewPlayerActivity extends Activity {
         }
     }
 }
+
 
