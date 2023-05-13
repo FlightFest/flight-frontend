2add0af046fcddd054bbda365cdaa2a14a724813
Arnau <arnaumoran@gmail.com>
2023-05-13 17:32:24 +0200

    lobby timer fix

--- app/src/main/java/LobbyActivity.java
+++ app/src/main/java/LobbyActivity.java
@@ -1,37 +1,37 @@
-import android.app.Activity;
-
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
 
-public class LobbyActivity extends Activity {
+import com.example.vuelin_go_front.R;
 
-    private static final long COUNTDOWN_TIME = 30000; // 30 seconds in milliseconds
+public class LobbyActivity extends AppCompatActivity {
 
-    private TextView countdownTimerTextView;
-    private CountDownTimer countDownTimer;
+    private TextView countdownTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lobby);
+        setContentView(R.layout.lobby);
 
-        countdownTimerTextView = findViewById(R.id.countdown_timer);
+        countdownTimer = findViewById(R.id.countdown_timer);
 
-        countDownTimer = new CountDownTimer(COUNTDOWN_TIME, 1000) {
-            @Override
+        // Set the countdown timer for 15 minutes (900,000 milliseconds)
+        new CountDownTimer(900000, 1000) {
+
             public void onTick(long millisUntilFinished) {
-                int secondsRemaining = (int) (millisUntilFinished / 1000);
-                countdownTimerTextView.setText(String.format("%02d:%02d", secondsRemaining / 60, secondsRemaining % 60));
+                // Update the countdown timer every second
+                long minutes = millisUntilFinished / 60000;
+                long seconds = (millisUntilFinished / 1000) % 60;
+                countdownTimer.setText(String.format("%02d:%02d", minutes, seconds));
             }
 
-            @Override
             public void onFinish() {
-                // TODO: handle countdown timer finish
+                // Do something when the countdown timer finishes
             }
-        };
 
-        countDownTimer.start();
+        }.start();
     }
 }
+
